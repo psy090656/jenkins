b26035b16c7ecdd3eec336ab2929317d0d6f39ab
SINYONUG PARK <psy090656@likelion.org>
2024-12-17 15:29:37 +0900

    init

--- src/main/java/src/main/java/com/Application.java
+++ src/main/java/src/main/java/com/Application.java
@@ -1,5 +1,8 @@
 package src.main.java.com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Application {
 
