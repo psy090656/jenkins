73ccc452ac5ba958273ececc6044d157a9af82a5
SINYONUG PARK <psy090656@likelion.org>
2024-12-17 13:38:59 +0900

    init

---
+++ src/main/java/src/main/java/com/Application.java
@@ -0,0 +1,9 @@
+package src.main.java.com;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
