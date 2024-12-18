7d054ac4b52c35ec33bf3974f41f500008191ce7
SINYONUG PARK <psy090656@likelion.org>
2024-12-18 10:36:50 +0900

    slack test

--- src/main/java/src/main/java/com/Application.java
+++ src/main/java/src/main/java/com/Application.java
@@ -9,5 +9,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("build Test");
+        System.out.println("slack Test");
     }
 }
